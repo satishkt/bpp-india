6e10fc1305d658a66c74d24af53f3595b202659d
Mandeepak <mandeepak@berkeley.edu>
2014-11-29 12:34:43 +0530

    Refactored Code - Added Generator

--- billion_prices_india/billion_prices_india/spiders/pricecheck_products.py
+++ billion_prices_india/billion_prices_india/spiders/pricecheck_products.py
@@ -5,20 +5,7 @@ import re
 from scrapy.log import ScrapyFileLogObserver
 import scrapy
 import json
-from scrapy.selector import HtmlXPathSelector
-from scrapy import log
 from billion_prices_india.items import BillionPricesIndiaItem
-from scrapy.selector import HtmlXPathSelector
-from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
-from scrapy.contrib.spiders import CrawlSpider, Rule
-from scrapy.http import Request
-from scrapy import log
-from os import path
-import os
-import urllib
-import string
-from bs4 import UnicodeDammit
-from urlparse import urlparse
 import logging
 import time
 
@@ -98,12 +85,6 @@ class PriceSpider(scrapy.Spider):
 
                   ]
 
-    AUTOTHROTTLE_ENABLED = True
-    AUTOTHROTTLE_DEBUG = True
-    DOWNLOAD_DELAY = 3
-    DOWNLOAD_TIMEOUT = 180
-    AUTOTHROTTLE_START_DELAY = 3
-    category=""
 
     def __init__(self, *args, **kwargs):
         ScrapyFileLogObserver(open("spider.log", 'w'), level=logging.INFO).start()
@@ -111,7 +92,6 @@ class PriceSpider(scrapy.Spider):
 
     def parse(self, response):
         results = json.loads(response.body)
-        items=[]
         for result in results['product']:
             item = BillionPricesIndiaItem()
             item['product'] = result['model']
@@ -126,8 +106,8 @@ class PriceSpider(scrapy.Spider):
                     item['measure']= 'pcs'
                     item['price']=price
                     item['unitprice']=price
-                    items.append(item)
-        return items
+                    yield item
+
 
 
 
