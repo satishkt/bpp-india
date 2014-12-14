8588851cb7437cce836a305971db81ecbbcc94f4
Mandeepak <mandeepak@berkeley.edu>
2014-12-14 10:33:31 +0530

    Sorting Items based on Price from Low to High for a given product

---
+++ billion_prices_india/vendor_price_sorting.py
@@ -0,0 +1,58 @@
+__author__ = 'mandeepa'
+
+
+from mrjob.job import MRJob
+import re
+import sys
+import operator
+from mrjob.protocol import JSONValueProtocol
+
+class MRPriceAnalysis(MRJob):
+
+    OUTPUT_PROTOCOL = JSONValueProtocol
+
+    def init_get_pprices(self):
+        self.pp = {}
+
+    def get_prices(self, _, line):
+
+        record=line.split(",")
+        if not len(record) is 6:
+             return
+        category,product,vendor,date,unitprice,quantity=record
+        if unitprice!='':
+            if unitprice[-1]=="L":
+                unitprice=float(unitprice[:-1])*100000
+            else:
+                unitprice=float(unitprice)
+        else:
+            unitprice=0.0
+        if product not in self.pp.keys():
+            self.pp[product]=[[vendor,unitprice]]
+        elif [vendor,unitprice] not in self.pp[product]:
+            self.pp[product].append([vendor,unitprice])
+
+
+    def sorted_get_prices(self):
+        for product, val in self.pp.iteritems():
+            flatVal=reduce(lambda x,y: x+y,val)
+            output={}
+            for store,price in val:
+                output.update({store:price})
+            sorted_output = sorted(output.items(), key=operator.itemgetter(1))
+            yield None,(product,sorted_output)
+
+
+
+    def steps(self):
+        return [self.mr(mapper_init=self.init_get_pprices,
+                        mapper=self.get_prices,
+                        mapper_final=self.sorted_get_prices
+
+                        )]
+
+
+if __name__ == '__main__':
+    MRPriceAnalysis.run()
+
+
